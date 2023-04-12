2e8760be4a3675c682c246f86ea0178ca823a8ae
Ankush Ingale <ankushingale958@gmail.com>
2023-04-12 17:56:22 +0530

    delete

--- DBMS_Project/src/main/java/com/deletecontrollers/deleteparty.java
+++ DBMS_Project/src/main/java/com/deletecontrollers/deleteparty.java
@@ -1,9 +1,6 @@
-package com.controller;
+package com.deletecontrollers;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,19 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.dao.adminoperations;
-import com.model.partymodel;
 
 /**
- * Servlet implementation class addpartycontroller
+ * Servlet implementation class deleteparty
  */
-@WebServlet("/addpartycontroller")
-public class addpartycontroller extends HttpServlet {
+@WebServlet("/deleteparty")
+public class deleteparty extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public addpartycontroller() {
+    public deleteparty() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -34,32 +30,14 @@ public class addpartycontroller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String party_id=request.getParameter("party_id");
 		
-		int user_id=0;
-		int i;
-		ArrayList<Integer> list=new ArrayList<Integer>();
-		for(i=1;i<5000;i++)
-		{
-			list.add(new Integer(i));
-		}
-		Collections.shuffle(list);
-		for(i=1;i<2000;i++)
-		{
-			user_id=list.get(i);
-		}
-		String party_id=Integer.toString(user_id);
-		String party_name=request.getParameter("pname");
-		String party_leader=request.getParameter("pleader");
-		String party_type=request.getParameter("ptype");
-		partymodel pm=new partymodel(party_id, party_name, party_leader, party_type);
 		adminoperations aop=new adminoperations();
-		int j=aop.addparty(pm);
-		if(j>0)
+		int i=aop.deleteParty(party_id);
+		if(i>0)
 		{
-			System.out.println("Record inserted");
+			System.out.println("Record deleted");
 		}
-		
-
 	}
 
 	/**
